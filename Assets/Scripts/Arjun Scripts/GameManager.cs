7a41b1ea07f170571229268a8b1633ca19b410c1
ArjunB30 <arjunb375@gmail.com>
2023-07-11 00:33:58 +0530

    Pause Menu Commit
    11-07-23 / 12:33am

--- Assets/Scripts/Arjun Scripts/GameManager.cs
+++ Assets/Scripts/Arjun Scripts/GameManager.cs
@@ -8,16 +8,53 @@ public class GameManager : MonoBehaviour
     public GameObject youWinScreen;
     public GameObject youLoseScreen;
     public GameObject UIImage;
+
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenu;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
+        GameIsPaused = true;
+
+    }
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+        GameIsPaused = false;
+    }
+
+  
     public void HomeButton()
     {
         SceneManager.LoadScene("MainMenu");
+        Time.timeScale = 1;
+
     }
     public void ReplayButton()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Time.timeScale = 1;
     }
     public void NextLevelButton()
     {
+
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
